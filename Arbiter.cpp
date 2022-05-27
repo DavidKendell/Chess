fee51d0206fa219cb719ebfdad58ae27d5f8112c
David Kendell <david.kendell@hotmail.co.uk>
2022-05-27 18:37:34 +0100

    minor bug fixes

--- Arbiter.cpp
+++ Arbiter.cpp
@@ -34,7 +34,7 @@ bool checkMove(Board::iterator boardBegin, Board::iterator newPiecePos,
 	case 'h':
 		return checkKing(oldPicePos, diff);
 	case 'P':
-		return checkKing(oldPicePos, diff);
+		return checkPawn(oldPicePos, diff, traverseDist);
 	case 'p':
 		return checkPawn(oldPicePos, diff, traverseDist);
 	default: return false;
@@ -139,8 +139,8 @@ bool checkPawn(Board::iterator piecePosition, int diff, int traverseDist)
 	bool black2step = 8 <= traverseDist && traverseDist <= 15 && diff == 16;
 	bool whiteTake = !checkEmpty(newPos) && (diff == -9 || diff == -7);
 	bool blackTake = !checkEmpty(newPos) && (diff == 9 || diff == 7);
-	bool legalWhite = piecePosition->isWhite && (white1step || white2step || whiteTake);
-	bool legalBlack = !piecePosition->isWhite && (black1step || black2step || blackTake);
+	bool legalWhite = piecePosition->isWhite && (white1step && checkEmpty(newPos) || white2step && checkEmpty(newPos) || whiteTake);
+	bool legalBlack = !piecePosition->isWhite && (black1step && checkEmpty(newPos) || black2step && checkEmpty(newPos) || blackTake);
     return legalBlack || legalWhite;
 }
 
